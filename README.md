91e9259d02ce217dc11ebb50a9a5094ae08ea6f2
thienan11 <76964183+thienan11@users.noreply.github.com>
2023-09-18 02:07:50 -0700

    working on ui

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # blogue
+A simple interactive blog site using MongoDB, Express.js, React.js, and Node.js.
+(currently in development)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      Welcome to Bloguetown
-    </div>
+    <main>
+      <header>
+        <a href="" className="logo">Bloguetown</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://zaigadusa.weebly.com/uploads/5/1/8/8/51882967/7940130_orig.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>What is the One Piece?</h2>
+          <p>One Piece, written by Eiichiro Oda, is a widely popular and critically acclaimed series revolving around the protagonist, Monkey D. Luffy, who currently embarks on an exciting quest to become the King of the Pirates.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://www.dexerto.com/cdn-cgi/image/width=1080,quality=75,format=auto/https://editors.dexerto.com/wp-content/uploads/2023/07/03/one-piece-netflix-luffy.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>One Piece: 5 reasons the anime is better than live-action</h2>
+          <p>One Piece live-action becomes a global sensation as soon as it premieres on Netflix as fans compare it with the anime series. Although it is an exceptional adaptation, there are just a few things that the anime does better.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src="https://zaigadusa.weebly.com/uploads/5/1/8/8/51882967/7940130_orig.jpg" alt=""/>
+        </div>
+        <div className="texts">
+          <h2>What is the One Piece?</h2>
+          <p>One Piece, written by Eiichiro Oda, is a widely popular and critically acclaimed series revolving around the protagonist, Monkey D. Luffy, who currently embarks on an exciting quest to become the King of the Pirates.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
