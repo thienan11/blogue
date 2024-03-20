1d18769f35c037ab959fa5e126b0c22085f86e72
ttran259 <ttran259@calpoly.edu>
2024-03-20 15:07:40 -0700

    add create new post endpoint

--- frontend/src/pages/CreatePostPage.js
+++ frontend/src/pages/CreatePostPage.js
@@ -1,45 +1,79 @@
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const modules = {
   toolbar: [
-    [{ 'header': [1, 2, false] }],
-    ['bold', 'italic', 'underline','strike', 'blockquote'],
-    [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-    ['link', 'image'],
-    ['clean']
-  ]
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
 };
 
 const formats = [
-  'header',
-  'bold', 'italic', 'underline', 'strike', 'blockquote',
-  'list', 'bullet', 'indent',
-  'link', 'image'
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
 ];
 
 export default function CreatePostPage() {
-  const [title, setTitle] = useState('');
-  const [summary, setSummary] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
+
+  async function createNewPost(ev) {
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set('file', files[0]); // grab first file (if theres multiple)
+    ev.preventDefault();
+    const response = await fetch("https://bloguetown-api.vercel.app/post", {
+      method: 'POST',
+      body: data,
+    });
+    console.log(await response.json());
+  }
 
   return (
-    <form className="create-post-form">
-      <input type="title" 
-             placeholder={"Title"} 
-             value={title} 
-             onChange={ev => setTitle(ev.target.value)}/>
-      <input type="summary" 
-             placeholder={"Summary"} 
-             value={summary}
-             onChange={ev => setSummary(ev.target.value)}/>
-      <input type="file" />
-      <ReactQuill 
-        value={content} 
-        onChange={newValue => setContent(newValue)}
-        modules={modules} 
-        formats={formats}/>
+    <form className="create-post-form" onSubmit={createNewPost}>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(ev) => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(ev) => setSummary(ev.target.value)}
+      />
+      <input 
+        type="file" 
+        onChange={(ev) => setFiles(ev.target.files)} />
+      <ReactQuill
+        value={content}
+        onChange={(newValue) => setContent(newValue)}
+        modules={modules}
+        formats={formats}
+      />
       <button>Create Post</button>
     </form>
   );
