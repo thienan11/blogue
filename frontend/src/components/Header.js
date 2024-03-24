b2c6372362407cf49e7651634450d458853498fb
ttran259 <ttran259@calpoly.edu>
2024-03-24 16:06:00 -0700

    add back register button to header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,62 +1,68 @@
-import { useNavigate, Link} from 'react-router-dom';
-import LoginButton from './LoginButton';
-import LogoutButton from './LogoutButton';
-import Profile from './Profile';
-import { useEffect, useState, useContext} from 'react';
-import { UserContext } from './UserContext';
+import { useNavigate, Link } from "react-router-dom";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import Profile from "./Profile";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "./UserContext";
 
 const Header = () => {
-  const {setUserInfo, userInfo} = useContext(UserContext);
+  const { setUserInfo, userInfo } = useContext(UserContext);
 
   useEffect(() => {
-    fetch('https://bloguetown-api.vercel.app/profile', {
-      credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
+    fetch("https://bloguetown-api.vercel.app/profile", { // http://localhost:4000/profile
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
         setUserInfo(userInfo);
       });
     });
-  }, [])
+  }, []);
 
   function logout() {
-    fetch('https://bloguetown-api.vercel.app/logout', {
-      credentials: 'include',
-      method: 'POST',
+    fetch("https://bloguetown-api.vercel.app/logout", { // http://localhost:4000/logout
+      credentials: "include",
+      method: "POST",
     });
     setUserInfo(null);
   }
 
   const username = userInfo?.username;
 
-  return(
+  return (
     <header>
       <div className="header-container">
-        <Link to="/" className="logo">Bloguetown</Link>
+        <Link to="/" className="logo">
+          {/* <img src="/blogue_final.svg" alt="Bloguetown Logo"/> */}
+          Bloguetown
+        </Link>
         <nav>
           {/* <Profile/>
           <LoginButton/>
           <LogoutButton/> */}
           {username && (
             <>
-              <Link className='button-4' to="/create">Create new post</Link>
-              <a className='button-4' onClick={logout}>Logout</a>
+              <Link className="button-4" to="/create">
+                Create new post
+              </Link>
+              <a className="button-4" onClick={logout}>
+                Logout
+              </a>
             </>
           )}
           {!username && (
             <>
-              <Link className="button-4" to="/login">Login</Link>
+              <Link className="button-4" to="/login">
+                Login
+              </Link>
+              <Link className="button-4" to="/register">
+                Register
+              </Link>
             </>
           )}
-          {/* <Link className="button-4">Register</Link> */}
         </nav>
       </div>
-      {/* <Link to="/" className="logo">Bloguetown</Link>
-      <nav>
-        <Link className="button-4" to="/login">Login</Link>
-        <Link className="button-4">Register</Link>
-      </nav> */}
     </header>
   );
-}
+};
 
 export default Header;
